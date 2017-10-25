aa6976ab4b15f99f4173da46196ec5cedbdfe67f
Manuel Piepereit <manuel.piepereit@neonpastell.de>
2017-10-25 10:46:07 +0200

    Update wpnp-sanitize-upload.php
    fixed issue #4 - again?

--- templates/wp-content/mu-plugins/wpnp-sanitize-upload.php
+++ templates/wp-content/mu-plugins/wpnp-sanitize-upload.php
@@ -3,55 +3,56 @@
 Plugin Name: Sanitize Uploads
 Plugin URI:
 Description: Sanitizes all filenames on upload
-Version: 1.0.0
+Version: 1.0.1
 Author: Neonpastell GmbH
 Author URI: http://www.neonpastell.de
- */
+*/
 
-if (!class_exists('NPWP_Sanitize_uploads')):
+if ( ! class_exists( 'NPWP_Sanitize_uploads' ) ) :
 
-	class NPWP_Sanitize_uploads {
+class NPWP_Sanitize_uploads {
 
-		public static function get_instance() {
-			static $instance = null;
+	public static function get_instance() {
+		static $instance = null;
 
-			if (null === $instance) {
-				$instance = new self();
-				add_filter('sanitize_file_name', array(__CLASS__, 'sanitize_filename_on_upload'), 10);
-			}
-
-			return $instance;
+		if ( null === $instance ) {
+			$instance = new self();
+			add_filter('sanitize_file_name', array( __CLASS__, 'sanitize_filename_on_upload'), 10);
 		}
 
-		/**
-		 * An empty constructor
-		 */
-		public function __construct() { /* Purposely do nothing here */}
-
-		function sanitize_filename_on_upload($filename) {
-			$explodedFilename = explode('.', $filename);
-			$ext = end($explodedFilename);
-			// Replace all special characters
-			$replace = array(
-				' ' => '_',
-				'Ä' => 'Ae',
-				'Ö' => 'Oe',
-				'Ü' => 'Ue',
-				'ä' => 'ae',
-				'ö' => 'oe',
-				'ü' => 'ue',
-				'ß' => 'ss',
-			);
-			$sanitized = strtr(substr($filename, 0, -(strlen($ext) + 1)), $replace);
-			// Replace all other weird characters
-			$sanitized = preg_replace('/[^a-zA-Z0-9-_.]/', '', $sanitized);
-			// Replace dots inside filename
-			$sanitized = str_replace('.', '-', $sanitized);
-			return strtolower($sanitized . '.' . $ext);
-		}
-
+		return $instance;
 	}
 
-	NPWP_Sanitize_uploads::get_instance();
+	/**
+	 * An empty constructor
+	 */
+	public function __construct() { /* Purposely do nothing here */ }
+
+	public static function sanitize_filename_on_upload($filename) {
+		$explodedFilename = explode('.', $filename);
+		$ext = end($explodedFilename);
+		// Replace all special characters
+		$replace = array(
+			' ' => '_',
+			'Ä' => 'Ae',
+			'Ö' => 'Oe',
+			'Ü' => 'Ue',
+			'ä' => 'ae',
+			'ö' => 'oe',
+			'ü' => 'ue',
+			'ß' => 'ss'
+		);
+		$sanitized = strtr(substr($filename, 0, -(strlen($ext)+1)), $replace);
+		// Replace all other weird characters
+		$sanitized = preg_replace('/[^a-zA-Z0-9-_.]/', '', $sanitized);
+		// Replace dots inside filename
+		$sanitized = str_replace('.','-', $sanitized);
+		return strtolower($sanitized.'.'.$ext);
+	}
+
+}
+
+
+NPWP_Sanitize_uploads::get_instance();
 
 endif;
